36b7d6c2290ebee35e2923d39a2ba389cd901ccf
DannyHayfield <dannyhayfield@gmail.com>
2016-10-31 10:35:21 +0000

    Revert "Padding"
    This reverts commit b7ef50ab6eb4f16e175c64f4d4fddd6aab03e68c.

--- offsets_new.php
+++ offsets_new.php
@@ -2,24 +2,24 @@
 ob_start("ob_gzhandler");
 
 $offsets = "";
-$offsets .= "LocalPlayer=0xA8F53C".",";				//LocalPlayer / m_dwLocalPlayer
-$offsets .= "EntityList=0x4AB0EF4".",";				//EntityList / m_dwEntityList
-$offsets .= "EnginePointer=0x5C22C4".",";			//EnginePointer / m_dwClientState
-$offsets .= "RadarBase=0x4EE5CCC".",";				//RadarBase / m_dwRadarBase
-$offsets .= "iGameResource=0x2E8CCCC".",";			//iGameResource
-$offsets .= "ScoreBoardBase=0x2E8CCCC".",";			//ScoreBoardBase
-$offsets .= "Glowptr=0x4FC8B04".",";				//m_dwGlowObject
-$offsets .= "jump=0x4F47A34".",";					//m_dwForceJump
-$offsets .= "Glowindex=0xA320".",";					//m_iGlowIndex
-$offsets .= "m_iCompetitiveRanking=0x1A44".",";		//m_iCompetitiveRanking
-$offsets .= "m_iCompetitiveWins=0x1B48".",";		//m_iCompetitiveWins
-$offsets .= "m_bIsDormant=0xE9".",";				//m_bDormant
-$offsets .= "c_dwBaseEntityPosition=0x134".",";		//m_vecOrigin
-$offsets .= "viewAngelOff=0x4D0C".",";				//m_dwViewAngles
-$offsets .= "m_iTeamNum=0xF0".",";					//m_iTeamNum
-$offsets .= "m_iHealth=0xFC".",";					//m_iHealth
-$offsets .= "m_iCrossHairID=0xAA64".",";			//m_iCrossHairID
-$offsets .= "m_hActiveWeapon=0x2EE8".",";			//m_hActiveWeapon
+$offsets .= "LocalPlayer=0x0A8F53C".",";				//LocalPlayer / m_dwLocalPlayer
+$offsets .= "EntityList=0x04AB0EF4".",";				//EntityList / m_dwEntityList
+$offsets .= "EnginePointer=0x05C22C4".",";			//EnginePointer / m_dwClientState
+$offsets .= "RadarBase=0x04EE5CCC".",";				//RadarBase / m_dwRadarBase
+$offsets .= "iGameResource=0x02E8CCCC".",";			//iGameResource
+$offsets .= "ScoreBoardBase=0x02E8CCCC".",";			//ScoreBoardBase
+$offsets .= "Glowptr=0x04FC8B04".",";				//m_dwGlowObject
+$offsets .= "jump=0x04F47A34".",";					//m_dwForceJump
+$offsets .= "Glowindex=0x00A320".",";					//m_iGlowIndex
+$offsets .= "m_iCompetitiveRanking=0x001A44".",";		//m_iCompetitiveRanking
+$offsets .= "m_iCompetitiveWins=0x001B48".",";		//m_iCompetitiveWins
+$offsets .= "m_bIsDormant=0x00E9".",";				//m_bDormant
+$offsets .= "c_dwBaseEntityPosition=0x00134".",";		//m_vecOrigin
+$offsets .= "viewAngelOff=0x004D0C".",";				//m_dwViewAngles
+$offsets .= "m_iTeamNum=0x00F0".",";					//m_iTeamNum
+$offsets .= "m_iHealth=0x00FC".",";					//m_iHealth
+$offsets .= "m_iCrossHairID=0x00AA64".",";			//m_iCrossHairID
+$offsets .= "m_hActiveWeapon=0x002EE8".",";			//m_hActiveWeapon
 $offsets .= "m_iWeaponID=0x2F88".",";				//m_iItemDefinitionIndex (!!NO LONGER m_iWeaponID)
 $offsets .= "m_fFlashMaxAlpha=0xA304".",";			//m_flFlashMaxAlpha
 $offsets .= "m_bSpotted=0x939".",";					//m_bSpotted
